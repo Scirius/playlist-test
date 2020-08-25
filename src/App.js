7ff4bd8c87c28db484f0ad96a811db03112cf711
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-25 14:32:20 +0200

    Added some playground elements for playlist

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from "react";
 import "./styles.css";
 import { Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import { IconButton } from "@material-ui/core";
+import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
+import PlaylistAddCheckIcon from "@material-ui/icons/PlaylistAddCheck";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import QueueMusicIcon from "@material-ui/icons/QueueMusic";
+import DeleteIcon from "@material-ui/icons/Delete";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
+import PlayCircleFilledWhiteIcon from "@material-ui/icons/PlayCircleFilledWhite";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import Tooltip from "@material-ui/core/Tooltip";
 
 const useStyles = makeStyles({
   myStyle: {
     backgroundColor: "red",
     fontStyle: "oblique",
     color: (props) => props.color
+  },
+  iconStyle: {
+    fontSize: "80px",
+    "& svg": {
+      fontSize: "80px"
+    }
   }
 });
 
@@ -27,6 +46,37 @@ export default function App() {
       <Button color="secondary" variant="outlined" fullWidth>
         Material UI Button
       </Button>
+      <div className={classes.iconStyle}>
+        <PlaylistAddIcon />
+        <PlaylistAddCheckIcon />
+        <PlaylistPlayIcon />
+        <QueueMusicIcon />
+        <DeleteIcon />
+        <DeleteForeverIcon />
+        <PlayArrowIcon />
+        <PauseIcon />
+        <PlayCircleFilledWhiteIcon />
+        <PlayCircleOutlineIcon />
+        <PauseCircleOutlineIcon />
+      </div>
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.button}
+        startIcon={<DeleteIcon />}
+      >
+        Delete
+      </Button>
+      <Tooltip title="Play">
+        <IconButton aria-label="play">
+          <PlayArrowIcon fontSize="large" />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Delete">
+        <IconButton aria-label="delete">
+          <DeleteIcon fontSize="large" />
+        </IconButton>
+      </Tooltip>
     </div>
   );
 }
