aad48b21732e43c29f86bb814bc0b80ca78d1ecb
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-25 12:41:00 +0200

    material UI button tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./styles.css";
-
+import { Button } from "@material-ui/core";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Button color="primary" variant="outlined">
+        Material UI Button
+      </Button>
+      <Button color="primary" variant="outlined" disabled>
+        Material UI Button
+      </Button>
+      <Button color="primary" variant="outlined" fullWidth>
+        Material UI Button
+      </Button>
     </div>
   );
 }
